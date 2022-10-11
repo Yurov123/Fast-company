6297f67611422db9fb08cf293ac830400530338e
konstantin <constantin8271@gmail.com>
2022-10-11 10:20:07 +0300

    add tableHeader

--- src/app/components/Users.jsx
+++ src/app/components/Users.jsx
@@ -6,13 +6,15 @@ import api from "../api";
 import GroupList from "./groupList";
 import SearchStatus from "./searchStatus";
 import UserTable from "./usersTable";
+import _ from 'lodash';
 
 const Users = ({ users: allUsers, ...rest }) => {
     const [currentPage, setCurrentPage] = useState(1);
     const [professions, setProfession] = useState();
     const [selectedProf, setSelectedProf] = useState();
+    const [sortBy, setSortBy] = useState({ iter: "name", order: "asc" });
 
-    const pageSize = 2;
+    const pageSize = 8;
     useEffect(() => {
         api.professions.fetchAll().then((data) => setProfession(data));
     }, []);
@@ -27,16 +29,22 @@ const Users = ({ users: allUsers, ...rest }) => {
     const handlePageChange = (pageIndex) => {
         setCurrentPage(pageIndex);
     };
+
+    const handleSort = (item) => {
+        setSortBy(item);
+    }
+
     const filteredUsers = selectedProf
         ? allUsers.filter(
-              (user) =>
-                  JSON.stringify(user.profession) ===
-                  JSON.stringify(selectedProf)
-          )
+            (user) =>
+                JSON.stringify(user.profession) ===
+                JSON.stringify(selectedProf)
+        )
         : allUsers;
 
     const count = filteredUsers.length;
-    const usersCrop = paginate(filteredUsers, currentPage, pageSize);
+    const sortedUsers = _.orderBy(filteredUsers, [sortBy.iter], [sortBy.order])
+    const usersCrop = paginate(sortedUsers, currentPage, pageSize);
     const clearFilter = () => {
         setSelectedProf();
     };
@@ -62,7 +70,11 @@ const Users = ({ users: allUsers, ...rest }) => {
             <div className="d-flex flex-column">
                 <SearchStatus length={count} />
                 {count > 0 && (
-                    <UserTable users={usersCrop} {...rest}/>
+                    <UserTable
+                        users={usersCrop}
+                        onSort={handleSort}
+                        selectedSort={sortBy}
+                        {...rest} />
                 )}
                 <div className="d-flex justify-content-center">
                     <Pagination
--- src/app/components/usersTable.jsx
+++ src/app/components/usersTable.jsx
@@ -1,33 +1,35 @@
-import { rest } from 'lodash';
+
 import React from 'react'
 import PropTypes from 'prop-types';
 import User from './user';
+import TableHeader from './tableHeader';
 
-
-const UserTable = ({users, ...rest}) => {
-    return <table className="table">
-    <thead>
-        <tr>
-            <th scope="col">Имя</th>
-            <th scope="col">Качества</th>
-            <th scope="col">Профессия</th>
-            <th scope="col">Встретился, раз</th>
-            <th scope="col">Оценка</th>
-            <th scope="col">Избранное</th>
-            <th />
-        </tr>
-    </thead>
-    <tbody>
-        {users.map((user) => (
-            <User {...rest} {...user} key={user._id} />
-        ))}
-    </tbody>
-</table>
+const UserTable = ({ users, onSort, selectedSort, ...rest }) => {
+    const columns = {
+        name: { iter: "name", name: "Имя" },
+        qualities: { name: "Качества" },
+        professions: { iter: "profession.name", name: "Профессия" },
+        completedMeetings: { iter: "completedMeetings", name: "Встретился, раз" },
+        rate: { iter: "rate", name: "Оценка" },
+        bookmark: { iter: "bookmark", name: "Избранное" },
+        delete: {}
+    }
+    return (
+        <table className="table">
+            <TableHeader {...{ onSort, selectedSort, columns }} />
+            <tbody>
+                {users.map((user) => (
+                    <User {...rest} {...user} key={user._id} />
+                ))}
+            </tbody>
+        </table>
+    );
 };
 UserTable.propTypes = {
     users: PropTypes.array.isRequired,
-
+    onSort: PropTypes.func.isRequired,
+    selectedSort: PropTypes.object.isRequired
 };
- 
+
 export default UserTable;
 
